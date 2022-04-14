c9abbc913b270faff78668c460507cbc3b4e6bd3
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-04-14 23:43:07 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    class Bike1 {
+        Bike1() {
+            System.out.println("Bike is READY");
+        }
+    }
+
+    public static void main(String[] args) {
+        Bike1 b=new Bike1();
+    }
+}
